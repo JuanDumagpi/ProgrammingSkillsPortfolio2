8184244eea2c1c330c19a44a3fcaf6a3753e227e
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2024-10-30 23:44:43 +0400

    Add files via upload
    Added sort and search functions to Student Manager

--- A1 - Skills Portfolio/Exercises/03StudentManager.py
+++ A1 - Skills Portfolio/Exercises/03StudentManager.py
@@ -11,18 +11,14 @@ with open ('studentMarks.txt') as file:
         data = line.strip().split(',') #takes out all the \n and separates the data into lines, and then each line into comma separated substrings
         database.append(data[0:])
 
-databaseUnsorted = database.copy()
-cMarkSorted = []
-for i in range(len(databaseUnsorted)):
-    sample = databaseUnsorted[0]
-    for x in databaseUnsorted:
-        if x[2]+x[3] < sample[2]+sample[3]:
-            sample = x
-    cMarkSorted.append(sample)
-    databaseUnsorted.remove(sample)
-    
-cMarkSort=cMarkSorted[::-1]
-
+#sorts by database list based on the highest total marks
+sortedByTotalM = database.copy()
+sortedByTotalMReverse = database.copy()
+sortedByTotalM.sort(key=lambda x: int(x[2]+x[3])) #ascending
+sortedByTotalMReverse.sort(key=lambda x: int(x[2]+x[3]), reverse=True) #descending
+#sort by name
+sortedByName = database.copy()
+sortedByName.sort(key=lambda x: x[1])
 
 root = Tk()
 root.title("Student Database")
@@ -49,12 +45,15 @@ myTree.heading("eMark", text="Exam Marks", anchor=W)
 myTree.heading("oPerc", text="Overall Percentage", anchor=W)
 myTree.heading("sGrade", text="Student Grade", anchor=W)
 
-#places our database on the treeview
+#places our database on the treeview based on name
 def placeData():
+    #Clears everything
+    for record in myTree.get_children():
+        myTree.delete(record)
     #adds everything
     global count
     count=0
-    for record in cMarkSort:
+    for record in sortedByName:
         if int(record[5]) >= 70:
             grade = " A"
         elif int(record[5]) < 70 and int(record[5]) >= 60:
@@ -67,11 +66,54 @@ def placeData():
             grade = " F"
         myTree.insert(parent='', index='end', iid=count, text="", values=(record[0], record[1], record[2], record[3], record[4] + "%", record[5] + grade))
         count+=1
-                 
+            
+#places our database on the treeview based on Lowest Total Marks
+def placeDataMarks():
+    #Clears everything
+    for record in myTree.get_children():
+        myTree.delete(record)
+    #adds everything
+    global count
+    count=0
+    for record in sortedByTotalM:
+        if int(record[5]) >= 70:
+            grade = " A"
+        elif int(record[5]) < 70 and int(record[5]) >= 60:
+            grade = " B"
+        elif int(record[5]) < 60 and int(record[5]) >= 50:
+            grade = " C"
+        elif int(record[5]) < 50 and int(record[5]) >= 40:
+            grade = " D"
+        else:
+            grade = " F"
+        myTree.insert(parent='', index='end', iid=count, text="", values=(record[0], record[1], record[2], record[3], record[4] + "%", record[5] + grade))
+        count+=1
 
+#places our database on the treeview based on Highest Total Marks
+def placeDataMarksR():
+    #Clears everything
+    for record in myTree.get_children():
+        myTree.delete(record)
+    #adds everything
+    global count
+    count=0
+    for record in sortedByTotalMReverse:
+        if int(record[5]) >= 70:
+            grade = " A"
+        elif int(record[5]) < 70 and int(record[5]) >= 60:
+            grade = " B"
+        elif int(record[5]) < 60 and int(record[5]) >= 50:
+            grade = " C"
+        elif int(record[5]) < 50 and int(record[5]) >= 40:
+            grade = " D"
+        else:
+            grade = " F"
+        myTree.insert(parent='', index='end', iid=count, text="", values=(record[0], record[1], record[2], record[3], record[4] + "%", record[5] + grade))
+        count+=1
     
 myTree.pack(pady=20)
-
+lenLabel=Label(root, text=studentTotal)
+lenLabel.pack()
 #Rearranges the treeview list based on the typed input in the entry box by Name
 def filterName(*args):
     items=myTree.get_children()
@@ -82,12 +124,17 @@ def filterName(*args):
             myTree.delete(eachItem)
             myTree.insert("",0,values=search_var)
 
-searchFrame = LabelFrame (root, text = "Filter by Name")
-searchFrame.pack(padx=10, pady=10)
 searchVar = StringVar()
-searchEntry = Entry(searchFrame, textvariable=searchVar)
+searchEntry = Entry(root, textvariable=searchVar)
+searchEntry.pack()
 searchVar.trace("w", filterName)
-searchEntry.pack (pady=20, padx=20, side = LEFT)
+
+button1=Button (root, text="Sort By Name", command=placeData)
+button2=Button (root, text="Sort By Highest Total Marks", command=placeDataMarksR)
+button3=Button (root, text="Sort By Lowest Total Marks", command=placeDataMarks)
+button1.pack()
+button2.pack()
+button3.pack()
 #-----------------------------------------------------------------------------------#
 
 
